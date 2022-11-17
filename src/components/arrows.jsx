3a2c0a11cc66375513784565dbb274350f6e6b9c
shaqdeff <shaquillendunda@gmail.com>
2022-11-17 22:58:23 +0300

    Arrow implementation

---
+++ src/components/arrows.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArrowOne from '../assets/arrow1';
+
+const Arrows = () => {
+  return (
+    <>
+      <ArrowOne style={{ marginTop: '500px', marginLeft: '850px' }} />
+    </>
+  );
+};
+
+export default Arrows;
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Navbar from '../components/navbar';
+import Arrows from '../components/arrows';
 
 const LandingPage = () => {
-  return <Navbar />;
+  return (
+    <div style={{ height: '100vh' }}>
+      <Navbar />
+      <Arrows />
+    </div>
+  );
 };
 
 export default LandingPage;
