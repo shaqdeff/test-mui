a63e15f4460a15ac27d401a5a7d58a3483a0be71
shaqdeff <shaquillendunda@gmail.com>
2022-11-17 21:01:48 +0300

    Create file structure and update README.md file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './Navbar';
+import LandingPage from './pages/landingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
+    <LandingPage />
   </React.StrictMode>
 );
